1aeca049a8f440b26fbadd170a000bb62618b1f4
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-01 01:24:12 +0530

    some more concepts project setup

--- context-api/src/components/userContext.js
+++ context-api/src/components/userContext.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+// const UserContext = React.createContext('Codevolution'); // it will work when there is no matching provider
 const UserContext = React.createContext();
 
 const UserProvider = UserContext.Provider

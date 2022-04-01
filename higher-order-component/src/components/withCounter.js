2e29f35a655bc64a8f3482e3a14f668c4bfb6ba3
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-01 15:42:58 +0530

    Added HOC 4

--- higher-order-component/src/components/withCounter.js
+++ higher-order-component/src/components/withCounter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
-const UpdatedComponent = (OriginalComponent) => {
-    class NewComponent extends Component {
+const withCounter = (WrappedComponent) => {
+    class WithCounter extends Component {
 
         constructor(props) {
             super(props)
@@ -19,11 +19,11 @@ const UpdatedComponent = (OriginalComponent) => {
 
         render() {
             return (
-                <OriginalComponent count={this.state.count} incrementCount={this.incrementCount} />
+                <WrappedComponent count={this.state.count} incrementCount={this.incrementCount} />
             )
         }
     }
-    return NewComponent
+    return WithCounter
 }
 
-export default UpdatedComponent
+export default withCounter

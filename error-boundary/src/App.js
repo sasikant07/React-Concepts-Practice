608ba83ddbe12d01984bab53d628a5d81c373786
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-01 17:44:32 +0530

    Added Error Boundary Concepts

--- error-boundary/src/App.js
+++ error-boundary/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ErrorBoundary from './components/ErrorBoundary';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ErrorBoundary>
+        <Hero heroName='Batman' />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName='Superman' />
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Hero heroName='Joker' />
+      </ErrorBoundary>
     </div>
   );
 }

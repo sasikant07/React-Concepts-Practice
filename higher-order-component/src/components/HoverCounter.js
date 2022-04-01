7552eec9afbc07cc51e0b682e5364b92554e37d7
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-01 14:55:21 +0530

    Added HOC 1

---
+++ higher-order-component/src/components/HoverCounter.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class HoverCounter extends Component {
+  render() {
+    return (
+      <h2>Hovered X times</h2>
+    )
+  }
+}
+
+export default HoverCounter

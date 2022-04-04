29d11ee753f0c68f9b75d3de3f0cfa0455e35bed
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-05 02:35:50 +0530

    Added complex custom hooks example

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -3,14 +3,19 @@ import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
 import DocTitleOne from './components/DocTitleOne';
 import DocTitleTwo from './components/DocTitleTwo';
+import UserForm from './components/UserForm';
 
 function App() {
   return (
     <div className="App">
       {/* <DocTitleOne />
       <DocTitleTwo /> */}
-      <CounterOne />
-      <CounterTwo />
+
+      
+      {/* <CounterOne />
+      <CounterTwo /> */}
+
+      <UserForm />
     </div>
   );
 }

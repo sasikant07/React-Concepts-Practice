895c622ffb479d0c966b052654f0225654a43ce8
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-04 18:55:13 +0530

    Data Fetching using useReducer hooks example 1

--- useReducer-data-fetching-hooks/src/App.js
+++ useReducer-data-fetching-hooks/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
 
 function App() {
   return (
     <div className="App">
-      App components
+      <DataFetchingOne />
     </div>
   );
 }

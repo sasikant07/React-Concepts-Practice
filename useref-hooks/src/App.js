b860aa7720c6c92b72a786814254edb7738ac01b
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-05 01:21:55 +0530

    Added useRef hooks basic example 2

--- useref-hooks/src/App.js
+++ useref-hooks/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import FocusInput from './components/FocusInput';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
-     <FocusInput />
+     {/* <FocusInput /> */}
+     <HookTimer />
     </div>
   );
 }

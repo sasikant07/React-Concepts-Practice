cdcdd2041accc396235d807f0d844c30ee1ef5d0
Sasikant Biswal <biswalsashikant@gmail.com>
2022-04-04 14:32:50 +0530

    useReducer example for multiple useReducer

--- usereducer-hooks/src/App.js
+++ usereducer-hooks/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import CounterOne from './components/CounterOne';
 import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
 
 function App() {
   return (
     <div className="App">
       {/* <CounterOne /> */}
-      <CounterTwo />
+      {/* <CounterTwo /> */}
+      <CounterThree />
     </div>
   );
 }
